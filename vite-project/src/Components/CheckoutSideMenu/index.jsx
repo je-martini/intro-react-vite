74d0f28aa4b610a172a9c1c9afa26b02aa3bc17e
je_martini <jesusmartini2017@gmail.com>
2024-04-21 09:02:48 -0400

    adding total sum of products

--- vite-project/src/Components/CheckoutSideMenu/index.jsx
+++ vite-project/src/Components/CheckoutSideMenu/index.jsx
@@ -3,6 +3,7 @@ import { XMarkIcon } from '@heroicons/react/24/solid'
 import { useContext } from "react"
 import { ShoppingCartContext } from "../../Context"
 import { OrderCard } from "../../Components/OrderCard"
+import { totalPrice } from "../../utils"
 
 
 const CheckoutSideMenu = () => {
@@ -22,7 +23,7 @@ const CheckoutSideMenu = () => {
         right-8 border border-black rounded-lg bg-white`
         }
     >
-
+        {/* Close Icon */}
     <div className="flex justify-between items-center p-6">
             <h2 className="font-medium text-xl">
                 My Order
@@ -33,7 +34,7 @@ const CheckoutSideMenu = () => {
 
             </XMarkIcon>
     </div>
-
+        {/* showing Products */}
     <div  className="px-6 mt-6 overflow-y-scroll">
         {
             context.productsInCart.map(product => (
@@ -44,9 +45,20 @@ const CheckoutSideMenu = () => {
                     imageURL={product.images}
                     price={product.price}
                     handleDelete={handleDelete}
+                    
+                    
                 />
+                
             ))
         }
+       
+    </div>
+
+    <div className="px-6 ">
+        <p className="flex justify-between items-center">
+            <span className="font-light">Total :</span>
+            <span className="font-medium text-xl">${totalPrice(context.productsInCart)}</span>
+        </p>
     </div>
     
     </aside>
